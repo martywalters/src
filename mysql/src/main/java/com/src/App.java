b4ca1c3fa8cb95c0b2501e9435531e1e4089412e
walters <walters@Ubuntu.myguest.virtualbox.org>
2023-08-09 15:40:32 -0700

    changes for get by id

--- mysql/src/main/java/com/src/App.java
+++ mysql/src/main/java/com/src/App.java
@@ -1,36 +1,14 @@
 package com.src;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Properties;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Hello World!");
-		Properties p=new Properties(); 
-		try {
-			FileReader reader=new FileReader("db.properties");
-		    //Properties p=new Properties();  
-		    p.load(reader);  
-		      
-		    System.out.println(p.getProperty("user"));  
-		    System.out.println(p.getProperty("password"));  
-			
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}  
-		
-		
-		
+
 		var db = Database.instance();
+		var p = Profile.getProperties("db");
 
 		try {
 			db.connect(p);
@@ -48,7 +26,7 @@ public class App {
 		if (userOpt.isPresent()) {
 			System.out.println("retrived: " + userOpt.get());
 			User user = userOpt.get();
-			user.setName("Marty");
+			user.setName("Steve");
 			userDao.update(user);
 		} else {
 			System.out.println("no user retrieved");
@@ -59,13 +37,8 @@ public class App {
 		} else {
 			System.out.println("no user retrieved");
 		}
-		
-		userDao.delete(new User(9,null));
-		
-		
-		
-		
 
+		userDao.delete(new User(9, null));
 		try {
 			db.close();
 		} catch (SQLException e) {
